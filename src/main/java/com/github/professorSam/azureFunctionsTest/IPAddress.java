db18acbbdbcb1084c3507c37b733206be54a0b43
ProfessorSam <76458774+ProfessorSam@users.noreply.github.com>
2023-01-24 01:15:48 +0100

    Add custom route

--- src/main/java/com/github/professorSam/azureFunctionsTest/IPAddress.java
+++ src/main/java/com/github/professorSam/azureFunctionsTest/IPAddress.java
@@ -8,13 +8,14 @@ public class IPAddress {
     @FunctionName("IPAddress")
     public HttpResponseMessage run(
             @HttpTrigger(
-                    name = "req", methods = {HttpMethod.GET},
-                    authLevel = AuthorizationLevel.ANONYMOUS)
+                    name = "req",
+                    methods = {HttpMethod.GET},
+                    authLevel = AuthorizationLevel.ANONYMOUS,
+                    route = "myip/")
             HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
-
-        String host = request.getHeaders().getOrDefault("X-Forwarded-For", "unkown");
+        context.getLogger().info("Function triggered");
+        String host = request.getHeaders().getOrDefault("x-forwarded-for", "unkown");
         return request.createResponseBuilder(HttpStatus.OK).body("Your ip address is " + host).build();
     }
 }
